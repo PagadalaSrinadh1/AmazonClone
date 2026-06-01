8b6745a0155dc1589ffcefe53d3454902cd642e3
PagadalaSrinadh1 <pagadalasrinadh1@gmail.com>
2026-06-01 19:19:25 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -105,11 +105,13 @@ export function loadProductsFetch() {
       });
 
       console.log("load products");
+    }).catch(() => {
+      console.log('Unexpected error. Please try again later.');
     });
 
     return promise;
 }
-/*
+
 loadProductsFetch().then(() => {
   console.log('next step');
 });
@@ -130,12 +132,15 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open("GET", "https://supersimplebackend.dev/products");
   xhr.send();
 }
 
-loadProducts();
-*/
+
 /*
 export const products = [
   {
